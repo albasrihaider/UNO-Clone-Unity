93b3d02780217879a953135936a515c9b65bd22c
Haider Albasri <haiderf3@gmail.com>
2024-08-19 02:32:22 +0300

    Add functionality to card script

--- Assets/Scripts/card.cs
+++ Assets/Scripts/card.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public string cardColor;
+	public string cardValue;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public SpriteRenderer cardRenderer;  // The SpriteRenderer for the card's background
+	
+	public void Initialize(string color, string value)
+	{
+		cardColor = color;
+		cardValue = value;
+
+		UpdateCardAppearance();
+	}
+
+	private void UpdateCardAppearance()
+	{
+		// Construct the sprite path based on the card color and value
+		string spritePath = $"UNO Cards/{cardColor}/{cardColor}_{cardValue}";
+
+		// Load the sprite from the Resources folder (assuming your sprites are there)
+		Sprite cardSprite = Resources.Load<Sprite>(spritePath);
+
+		// Check if the sprite was found
+		if (cardSprite != null)
+		{
+			cardRenderer.sprite = cardSprite;
+		}
+		else
+		{
+			Debug.LogError($"Sprite '{spritePath}' not found! Make sure the naming convention is correct.");
+		}
+
+	}
+
+	private void OnMouseDown()
+	{
+		// Handle card selection logic
+		Debug.Log("Card Selected: " + cardColor + " " + cardValue);
+		// Implement the logic to play this card
+	}
 }
+
